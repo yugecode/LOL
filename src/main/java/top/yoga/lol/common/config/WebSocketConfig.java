f36c3753be20ab984f25a84af3beb5171a29f932
Yuge <ljy_0624@qq.com>
2020-02-26 10:12:20 +0800

    修改时间和请求三方接口方式

---
+++ src/main/java/top/yoga/lol/common/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package top.yoga.lol.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+
+/**
+ * websocket配置类
+ *
+ * @author luojiayu
+ * @date 2020/2/25 14:06
+ */
+//@Component
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
---
+++ src/main/java/top/yoga/lol/common/service/WebSocketServer.java
@@ -0,0 +1,16 @@
+package top.yoga.lol.common.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * websocket服务类
+ *
+ * @author luojiayu
+ * @date 2020/2/26 9:24
+ */
+@Slf4j
+
+public class WebSocketServer {
+
+
+}
