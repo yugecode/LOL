9b5f6773966da890b292d43a4fb84ce0f978d8ba
Yuge <ljy_0624@qq.com>
2020-03-01 15:07:10 +0800

    WebSocket

--- src/main/java/top/yoga/lol/common/config/WebSocketConfig.java
+++ src/main/java/top/yoga/lol/common/config/WebSocketConfig.java
@@ -1,9 +1,19 @@
 package top.yoga.lol.common.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.session.StandardSessionFacade;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+import javax.servlet.http.HttpSession;
+import javax.websocket.HandshakeResponse;
+import javax.websocket.server.HandshakeRequest;
+import javax.websocket.server.ServerEndpoint;
+import javax.websocket.server.ServerEndpointConfig;
 
 
 /**
@@ -12,11 +22,12 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
  * @author luojiayu
  * @date 2020/2/25 14:06
  */
-//@Component
+@Slf4j
+@Component
 @Configuration
 public class WebSocketConfig {
 
-//    @Bean
+    @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
     }
--- src/main/java/top/yoga/lol/common/service/WebSocketServer.java
+++ src/main/java/top/yoga/lol/common/service/WebSocketServer.java
@@ -1,10 +1,7 @@
 package top.yoga.lol.common.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import top.yoga.lol.user.dao.UserDao;
-import top.yoga.lol.user.entity.User;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
@@ -14,9 +11,7 @@ import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * websocket服务类
@@ -24,61 +19,35 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author luojiayu
  * @date 2020/2/26 9:24
  */
-@Slf4j
+@ServerEndpoint(value = "/websocket/{sid}")
 @Component
-@ServerEndpoint(value = "/webSocket/{userId}")
+@Slf4j
 public class WebSocketServer {
 
-    @Autowired
-    private UserDao userDao;
+    //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
+    private static int onlineCount = 0;
+    //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
+    private static CopyOnWriteArraySet<WebSocketServer> webSocketSet = new CopyOnWriteArraySet<WebSocketServer>();
 
-    //用于存放每个客户端存对应的WebSocketServer对象
-    private static ConcurrentHashMap<Integer, Session> webSocketSet = new ConcurrentHashMap();
+    //与某个客户端的连接会话，需要通过它来给客户端发送数据
+    private Session session;
 
+    //接收sid
+    private String sid="";
     /**
-     * 连接成功调用的方法
-     *
-     * @param userId  用户id
-     * @param session 用户对应的session
-     */
+     * 连接建立成功调用的方法*/
     @OnOpen
-    public void onOpen(@PathParam("userId") Integer userId, Session session) {
-        webSocketSet.put(userId,session);
-        User user = userDao.getUserById(userId);
-        List<String> totalPushMsgs = new ArrayList();
-        totalPushMsgs.add("欢迎您:"+ user.getUserName());
-        if(totalPushMsgs != null && !totalPushMsgs.isEmpty()) {
-            totalPushMsgs.forEach(e -> this.sendMessage(userId,e));
-        }
-    }
-
-    public void sendMessage(Integer userId,String message) {
-        try {
-            Session currentSession = webSocketSet.get(userId);
-            if(currentSession != null){
-                currentSession.getBasicRemote().sendText(message);
-            }
-            log.info("推送消息成功，消息为：" + message);
+    public void onOpen(Session session,@PathParam("sid") String sid) {
+        this.session = session;
+        webSocketSet.add(this);     //加入set中
+        addOnlineCount();           //在线数加1
+        log.info("有新窗口开始监听:"+sid+",当前在线人数为" + getOnlineCount());
+        this.sid=sid;
+        /*try {
+            sendMessage(JSON.toJSONString(RestResponse.success()));
         } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    /**
-     * 自定义消息
-     */
-    public static void sendInfo(String userId,String message) throws IOException {
-        Session currentSession = webSocketSet.get(userId);
-        if(currentSession != null){
-            currentSession.getBasicRemote().sendText(message);
-        }
-    }
-
-    /**
-     * 用户退出时，连接关闭调用的方法
-     */
-    public static void onCloseConection(String userId) {
-        webSocketSet.remove(userId); // 从set中删除
+            log.error("websocket IO异常");
+        }*/
     }
 
     /**
@@ -86,24 +55,84 @@ public class WebSocketServer {
      */
     @OnClose
     public void onClose() {
-        log.info("一个客户端关闭连接");
+        webSocketSet.remove(this);  //从set中删除
+        subOnlineCount();           //在线数减1
+        log.info("有一连接关闭！当前在线人数为" + getOnlineCount());
     }
 
     /**
      * 收到客户端消息后调用的方法
-     * @param message
-     * 客户端发送过来的消息
-     */
+     *
+     * @param message 客户端发送过来的消息*/
     @OnMessage
     public void onMessage(String message, Session session) {
-
+        //log.info("收到来自窗口"+sid+"的信息:"+message);
+        if("heart".equals(message)){
+            try {
+                sendMessage("heartOk");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        //群发消息
+       /* for (WebSocketServer item : webSocketSet) {
+            try {
+                item.sendMessage(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }*/
     }
 
     /**
-     * 发生错误时调用
+     *
+     * @param session
+     * @param error
      */
     @OnError
     public void onError(Session session, Throwable error) {
-        log.error("websocket出现错误");
+        log.error("发生错误");
+        error.printStackTrace();
     }
+    /**
+     * 实现服务器主动推送
+     */
+    public void sendMessage(String message) throws IOException {
+        this.session.getBasicRemote().sendText(message);
+    }
+
+
+    /**
+     * 群发自定义消息
+     * */
+    public static void sendInfo(String message) throws IOException {
+
+        for (WebSocketServer item : webSocketSet) {
+            try {
+                //这里可以设定只推送给这个sid的，为null则全部推送
+//                if(sid==null) {
+
+                item.sendMessage(message);
+                log.info("推送消息到窗口"+item.sid+"，推送内容:"+message);
+//                }else if(item.sid.equals(sid)){
+//                    item.sendMessage(message);
+//                }
+            } catch (IOException e) {
+                continue;
+            }
+        }
+    }
+
+    public static synchronized int getOnlineCount() {
+        return onlineCount;
+    }
+
+    public static synchronized void addOnlineCount() {
+        WebSocketServer.onlineCount++;
+    }
+
+    public static synchronized void subOnlineCount() {
+        WebSocketServer.onlineCount--;
+    }
+
 }
